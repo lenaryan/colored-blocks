ac8654bdd1f584a5a40629e0382c652aad61d362
Lena Ryan <alissadyiphil@gmail.com>
2020-08-26 21:33:23 +0300

    added main blocks + header structure

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <a href="/"><img src="" alt="Logo"></a>
+        <TopMenu />
+        <SignUp />
+    </header>
+</template>
+
+<script>
+import TopMenu from '@/components/TopMenu';
+import SignUp from '@/components/SignUp';
+export default {
+    name: 'app',
+    components: {
+        TopMenu,
+        SignUp
+    }
+}
+</script>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,5 @@
+<template>
+    <button>
+        Sign Up
+    </button>
+</template>
