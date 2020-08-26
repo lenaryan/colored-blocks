5c63f0e33a52053c493eac596f8082109b04e5f5
Lena Ryan <alissadyiphil@gmail.com>
2020-08-26 22:35:24 +0300

    footer structure

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,19 @@
 <template>
     <footer>
-        
+        <a href="/"><img src="" alt="Logo"></a>
+        <BottomMenu />
+        <Form />
     </footer>
 </template>
+
+<script>
+import BottomMenu from '@/components/BottomMenu';
+import Form from '@/components/Form';
+export default {
+    name: 'app',
+    components: {
+        BottomMenu,
+        Form
+    }
+}
+</script>
