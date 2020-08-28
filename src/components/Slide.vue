ed207275753465f8947ab26620bf7cdc30417dc6
Lena Ryan <alissadyiphil@gmail.com>
2020-08-28 23:14:19 +0300

    deleted slidefeature component

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -1,16 +1,5 @@
 <template>
     <div>
-        <SlideFeature />
-        <SlideFeature />
+        
     </div>
 </template>
-
-<script>
-import SlideFeature from '@/components/SlideFeature';
-export default {
-    name: 'app',
-    components: {
-        SlideFeature
-    }
-}
-</script>
