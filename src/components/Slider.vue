62e090cfce1eb0860aa28115f3d5085fbd81a185
Lena Ryan <alissadyiphil@gmail.com>
2020-08-30 14:12:19 +0300

    slider dots mockup + slider script

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,8 +1,24 @@
 <template>
     <section class="slider">
-        <ul class="slider__wrapper">
-            <Slide v-for="feature in features"
-            :feature="feature" :key="feature.id" />
+        <div class="slider__wrapper">
+            <ul class="slider__slides">
+                <Slide v-for="feature in features"
+                :feature="feature" :key="feature.id" />
+            </ul>
+        </div>
+        <ul class="slider__dots">
+            <li class="slider__dot">
+                <button class="slider__btn slider__btn--0" @click="move(0)"></button>
+            </li>
+            <li class="slider__dot">
+                <button class="slider__btn slider__btn--1" @click="move(1)"></button>
+            </li>
+            <li class="slider__dot">
+                <button class="slider__btn slider__btn--2" @click="move(2)"></button>
+            </li>
+            <li class="slider__dot">
+                <button class="slider__btn slider__btn--3" @click="move(3)"></button>
+            </li>
         </ul>
     </section>
 </template>
@@ -14,6 +30,32 @@ export default {
     props: ['features'],
     components: {
         Slide
+    },
+    methods: {
+        move: function(num) {
+            //document.querySelector('.slider__slides').style.transform = 'translateX(' + (-num * 100) + '%)';
+            let percents;
+            let sliderStyle = document.querySelector('.slider__slides').getAttribute('style');
+            if (sliderStyle == null) {
+                percents = 0;
+            } else {
+                percents = Math.abs(parseInt(document.querySelector('.slider__slides').getAttribute('style').slice(22,-3)));
+            }
+
+            console.table(percents, num)
+
+            if (percents < (num * 100)) {
+                console.table('percents less', percents, num)
+                for (let px = percents; px <= (num * 100); px++) {
+                    document.querySelector('.slider__slides').style.transform = 'translateX(' + -px + '%)';
+                }
+            } else if (percents > (num * 100)) {
+                console.table('percents more',percents, num)
+                for (let px = percents; px >= (num * 100); px--) {
+                    document.querySelector('.slider__slides').style.transform = 'translateX(' + -px + '%)';
+                }
+            }
+        }
     }
 }
 </script>
